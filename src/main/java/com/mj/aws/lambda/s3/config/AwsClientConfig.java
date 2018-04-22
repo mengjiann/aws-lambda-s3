3d8dbcbfbed02472bc41f0dc7742b0e66f944280
leemj <leemj@sph.com.sg>
2018-04-22 22:12:50 +0800

    Added support for local deployment using same local with localstack

--- src/main/java/com/mj/aws/lambda/s3/config/AwsClientConfig.java
+++ src/main/java/com/mj/aws/lambda/s3/config/AwsClientConfig.java
@@ -1,16 +1,28 @@
 package com.mj.aws.lambda.s3.config;
 
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class AwsClientConfig {
 
     @Bean
+    @Profile("aws")
     public AmazonS3 awsS3Client(){
         return AmazonS3ClientBuilder.standard().build();
+    }
+
+    @Bean
+    @Profile("local")
+    public AmazonS3 awsS3ClientLocalStack(){
+        return AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder
+                        .EndpointConfiguration("http://localstack:4572","us-west-1"))
+                .build();
     }
 
 }
